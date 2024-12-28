5557db422e962c1baef5148b5dc11cd0ea05f64b
Sakshi Singh <sakshi23042005@gmail.com>
2024-12-28 07:24:06 +0000

    Change Banckround Color every second on clicking start

---
+++ bgColorChange.js
@@ -0,0 +1,32 @@
+//Generate a random color
+
+const randomColor=function(){
+    const hex='0123456789ABCDEF';
+    let color='#';
+    for(let i=0; i<6; i++){
+  
+      //math.floor.random generates random position for hexadecimal code to generate random color
+      color+=hex[Math.floor(Math.random()*16)];
+    }
+    return color;
+  };
+  
+  // console.log(randomColor());
+  let intervalId;
+  const startChangingColor=function(){
+    
+      if(!intervalId){
+      intervalId=setInterval(changeBgColor, 1000);
+      function changeBgColor(){
+      document.body.style.backgroundColor=randomColor();
+      }
+    }
+  };
+  
+  const stopChangingColor=function(){
+    clearInterval(intervalId);
+    intervalId=null;
+  }
+  document.querySelector("#start").addEventListener('click', startChangingColor);
+  
+  document.querySelector("#stop").addEventListener('click', stopChangingColor);
